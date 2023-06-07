3309d68f6952a8b8717a71bcc7123624273dc9de
vlady <vladimirstoqnov7@gmail.com>
2023-06-07 22:50:36 +0300

    Adding skills

--- vlady_cv/settings.py
+++ vlady_cv/settings.py
@@ -137,3 +137,6 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_PROXY_SSL_HEADER = ('HTTPS_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+
